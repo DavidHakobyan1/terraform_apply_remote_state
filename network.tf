252ab705bc888d727fed57a218ea096764c22673
Ani Avetisyan <avetisyan.ani97@mail.ru>
2023-01-12 14:47:52 +0400

    fixed errors

--- network.tf
+++ network.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-  access_key = ${{ secrets.AWS_ACCESS_KEY_ID }}
-  secret_key = ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+  access_key = var.access_key
+  secret_key = var.secret_key
   region = "us-east-1"
 }
 
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "access_key" {
+  type = string
+  default = "AKIARBXZQODDIHPAZHBU"
+}
+
+variable "secret_key" {
+  type = string
+  default = "jDJUjOkHge+OmvQwXyYB7qa4777QF8PTpDs7u5Hy"
+}
