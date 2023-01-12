7099c968ac294f87c412e7f2a1965df9a28dc6c7
DavidHakobyan1 <96466997+DavidHakobyan1@users.noreply.github.com>
2023-01-12 17:01:01 +0400

    Delete variables.tf

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "access_key" {
-  type = string
-  default = "AKIARBXZQODDIHPAZHBU"
-}
-
-variable "secret_key" {
-  type = string
-  default = "jDJUjOkHge+OmvQwXyYB7qa4777QF8PTpDs7u5Hy"
-}
