9bc6b35e615746ce6235541970131f55f6bb543e
Ani Avetisyan <avetisyan.ani97@mail.ru>
2023-01-12 13:58:31 +0400

    added new network.tf

---
+++ network.tf
@@ -0,0 +1,41 @@
+provider "aws" {
+  access_key = ${{ secrets.AWS_ACCESS_KEY_ID }}
+  secret_key = ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+  region = "us-east-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = aws_s3_bucket.my_bucket.bucket
+    key    = "dev/network/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+resource "aws_instance" "my_ubuntu" {
+  ami           = "ami-06878d265978313ca"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name    = "My Ubuntu server"
+    Owner   = "David Hakobyan"
+    Project = "Terraform Homework"
+  }
+}
+
+resource "aws_s3_bucket" "my_bucket" {
+  bucket = "david-terraform-bucket-hakobyan"
+  versioning {
+    enabled = true
+    acl     = "private"
+  }
+  tags = {
+    Name        = "My s3 bucket"
+    Environment = "Dev"
+  }
+}
+
+output "s3_bucket_name" {
+  value = aws_s3_bucket.my_bucket.id
+}
+
