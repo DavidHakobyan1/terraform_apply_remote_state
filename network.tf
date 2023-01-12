d65f47f74e003024ce1d357efbf5574df776de6c
Ani Avetisyan <avetisyan.ani97@mail.ru>
2023-01-12 15:03:05 +0400

    fixed errors

--- network.tf
+++ network.tf
@@ -4,14 +4,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-terraform {
-  backend "s3" {
-    bucket = "david-terraform-bucket-hakobyan"
-    key    = "dev/network/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
-
 resource "aws_instance" "my_ubuntu" {
   ami           = "ami-06878d265978313ca"
   instance_type = "t2.micro"
@@ -33,6 +25,14 @@ resource "aws_s3_bucket" "my_bucket" {
   tags = {
     Name        = "My s3 bucket"
     Environment = "Dev"
+  }
+}
+
+terraform {
+  backend "s3" {
+    bucket = "david-terraform-bucket-hakobyan"
+    key    = "dev/network/terraform.tfstate"
+    region = "us-east-1"
   }
 }
 
