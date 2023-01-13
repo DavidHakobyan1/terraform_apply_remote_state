6fd66243a5b75e9b6cfc7d2119b4289fccb192b1
Ani Avetisyan <avetisyan.ani97@mail.ru>
2023-01-13 17:56:59 +0400

    added name instance

--- main.tf
+++ main.tf
@@ -21,4 +21,9 @@ resource "aws_s3_bucket" "terraform_state" {
 resource "aws_instance" "my_ubuntu" {
   ami           = "ami-06878d265978313ca"
   instance_type = "t2.micro"
+  tags = {
+    Name    = "My Ubuntu server"
+    Owner   = "David Hakobyan"
+    Project = "Terraform Homework"
+  }
 }
